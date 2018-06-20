dc3b9bef7d1f413033386c0dc35291d579cb5867
Umer Khalid <umer12khalid@gmail.com>
2018-06-20 12:10:55 -0700

    Version 1.0

---
+++ PlacesToRemember/app/src/main/java/com/example/pc_home/placestoremember/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.pc_home.placestoremember;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    public static ListView listView;
+    public static ArrayList<String> arrayList = new ArrayList<String>();
+    public static ArrayAdapter arrayAdapter;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.LocationNames);
+
+
+        arrayList.add("Add new places...");
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("LocationIndex", position);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
